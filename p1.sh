6c964618b7a1ad2c73d039a349b78857ae1703bb
alex <alex>
2019-05-06 15:16:13 -0500

    updated js

--- p1.sh
+++ p1.sh
@@ -2,10 +2,6 @@
 az group create --location southcentralus --name myResourceGroup
 
 
-#create appservice plan
-az appservice plan create --name myPlan --resource-group myResourceGroup --is-linux --location southcentralus --number-of-workers 3 --sku B1
-
-
 #create storage account
 az storage account create --myStorageAccount -g myResourceGroup --access-tier hot --location southcentralus
 
@@ -17,11 +13,25 @@ blobStorageAccountKey=$(az storage account keys list -g myResourceGroup \
 az storage container create -n myBlobStorage --account-name myStorageAccount \
 --account-key $blobStorageAccountKey
 
+#create appservice plan
+az appservice plan create --name myPlan --resource-group myResourceGroup --is-linux --location southcentralus --number-of-workers 3 --sku B1
+
 
 #create web app and links git repo
 az webapp create --name myWebApp -g myResourceGroup --plan myPlan -b master \
 -u https://github.com/alexvo19/revature-p1
 
-#create db and link to web app
 
+#create cosmos account
+az cosmosdb create --name myCosmosAcc -g myResourceGroup
+
+
+#create database
+az cosmosdb database create --name myCosmosAcc -g myResourceGroup \
+--db-name myCosmosDB
+
+
+#create container
+az cosmosdb collection create -g myResourceGroup --collection-name myCollection \
+--name myCosmosAcc --db-name myCosmosDB
 
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const multer = require('multer');
+const upload = multer({
+  dest: 'uploads/' 
+}); 
+
+const app = express();
+
+
+//handles html
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+//post to handle upload
+app.post('/', upload.single('file-to-upload'), (req, res) => {
+  res.redirect('/');
+});
+
+//app.listen(3000);
+
+//post to handle upload
+// router.post('/', uploadStrategy, async (req, res) => {
+
+//   const aborter = Aborter.timeout(30 * ONE_MINUTE);
+//   const blobName = getBlobName(req.file.originalname);
+//   const stream = getStream(req.file.buffer);
+//   const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
+//   const blockBlobURL = BlockBlobURL.fromContainerURL(containerURL, blobName);
+
+//   try {
+    
+//     await uploadStreamToBlockBlob(aborter, stream,
+//       blockBlobURL, uploadOptions.bufferSize, uploadOptions.maxBuffers);
+
+//     res.render('success', { message: 'File uploaded to Azure Blob storage.' });   
+
+//   } catch (err) {
+
+//     res.render('error', { message: err.message });
+
+//   }
+// });
+
+// module.exports = router;
